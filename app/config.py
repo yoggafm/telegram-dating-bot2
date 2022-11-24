082b75d58b89412b1ae7bf36123b6a6722571416
yoggafm <118955791+yoggafm@users.noreply.github.com>
2022-11-24 15:37:22 +0700

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,8 +2,8 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-ADMIN = os.getenv('ADMIN').split(' ')  # add admin IDs separated by spaces in .env file
+BOT_TOKEN = os.getenv('5728932780:AAFQ-Hz0QAQHIV7j7s8hiH0n6Fj9adfKfhQ')
+ADMIN = os.getenv('JualVIPVVIP').split(' ')  # add admin IDs separated by spaces in .env file
 
 DB_HOST = os.getenv('DB_HOST')
 DB_USER = os.getenv('DB_USER')
